578a1074310e8057e2b4c7a9c628a5e46cef3f35
scobca <scobca18@yandex.ru>
2023-08-04 23:44:03 +0300

    creating VInput

---
+++ src/components/UI/VInput.vue
@@ -0,0 +1,34 @@
+<template>
+    <input :placeholder="placeholder"
+           class="input"
+           @input="$emit('update:modelValue', $event.target.value)"
+    >
+</template>
+
+<script lang="ts">
+
+import {Options, Vue} from "vue-class-component";
+import {Prop} from "vue-property-decorator";
+
+@Options({
+    name: 'VInput'
+})
+export default class VInput extends Vue {
+    @Prop({})
+    placeholder!: string
+}
+</script>
+
+<style scoped>
+
+.input {
+    height: 1.5rem;
+    width: inherit;
+    border-radius: .5rem;
+
+
+    text-align: center;
+    color: var(--text-color);
+    background-color: var(--input-bg-color);
+}
+</style>
