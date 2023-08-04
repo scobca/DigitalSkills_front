dabea0bd61215c2411f0f9164e876d36c38613fa
scobca <scobca18@yandex.ru>
2023-08-04 18:15:26 +0300

    starting developing login page

---
+++ src/views/AuthView.vue
@@ -0,0 +1,19 @@
+<template>
+    <p> AuthView </p>
+</template>
+
+<script lang="ts">
+
+import {Options, Vue} from "vue-class-component";
+
+@Options({
+    name: 'AuthView'
+})
+export default class AuthView extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
