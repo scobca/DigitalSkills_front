7d7712fea424665556f3b0be24df4dc2df038acf
scobca <scobca18@yandex.ru>
2023-08-09 17:27:21 +0300

    Comments

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
+//application
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import {store} from "@/store/store";
 
+//vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+//styles
 import '@/assets/styles/main.css'
+import '@/assets/styles/colors.css'
 
 const vuetify = createVuetify({
     components,
