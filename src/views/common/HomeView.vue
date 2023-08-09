52399e67f4efc7d0fd1dca66733ffbe6bd0ccc4a
scobca <scobca18@yandex.ru>
2023-08-10 02:29:46 +0300

    More animations (second iteration)

--- src/views/common/HomeView.vue
+++ src/views/common/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container main">
         <div class="greetings_container">
-            <div class="header animate__animated animate__backInDown">
+            <div class="header greetings_main_header animate__animated animate__zoomIn">
                 <h1 class="header">Ковалёво inc. &mdash; </h1>
                 <h3 class="header">биллинговая компания номер 1 в России</h3>
             </div>
             <div class="description">
                 <p class="description_text">
-                    Ковалёво Inc. – это инновационная биллинговая компания, специализирующаяся на предоставлении
+                    <b>Ковалёво Inc.</b> – это инновационная биллинговая компания, специализирующаяся на предоставлении
                     высококачественных решений для автоматизации и оптимизации финансовых процессов различных бизнесов.
                 </p>
             </div>
@@ -20,6 +20,9 @@
 <!--    TODO create new icon - arrow_down-->
     <div class="arrow_down_container">
         <img src="../../assets/svg/arrow_icon_down.svg" alt="" class="arrow_down animate__animated animate__flash animate__delay-2s	">
+    </div>
+    <div class="benefits_container">
+        <h1 class="benefits_header animate__animated animate__backInUp animate__slow animate__delay-2s">Наши преимущества</h1>
     </div>
 </template>
 
@@ -70,6 +73,29 @@ export default class HomeView extends Vue {
     align-items: center;
 }
 
+.benefits_container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 2rem 0;
+}
+
+.benefits_header {
+    margin: 0 0 0 -13%;
+    letter-spacing: .5rem;
+    font-size: 2.25rem;
+    width: max-content;
+    border-bottom: .25rem solid transparent;
+    scale: 100%;
+    transition-duration: 1s;
+}
+
+.benefits_header:hover {
+    border-bottom: .25rem solid var(--text-color);
+    scale: 110%;
+}
+
 .header {
     width: 20rem;
     text-align: left;
@@ -78,6 +104,15 @@ export default class HomeView extends Vue {
 .carousel_header {
     font-size: 2rem;
     margin-bottom: 1rem;
+}
+
+.greetings_main_header {
+    scale: 110%;
+    transition: .75s;
+}
+
+.greetings_main_header:hover {
+    scale: 120%;
 }
 
 .description {
@@ -91,11 +126,7 @@ export default class HomeView extends Vue {
     gap: 2rem;
 }
 
-.animate__backInDown {
-    --animate-duration: 1.5s;
-}
-
 .animate__flash{
-    --animate-duration: 3s;
+    --animate-duration: 2.5s;
 }
 </style>
