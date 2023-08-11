2422540e942c5cda3f0eb590abe66ffb888bc3cf
scobca <scobca18@yandex.ru>
2023-08-11 22:55:42 +0300

    new classes working with FormData

--- src/api/models/AuthModel.ts
+++ src/api/models/AuthModel.ts
@@ -1,12 +1,95 @@
-import ApiResolver from "@/api/ApiResolver";
+import axios from "axios";
 
-export class AuthModel extends ApiResolver {
+import ApiResolver from "@/api/ApiResolver";
+import apiConf from "@/api/api.conf";
+import {store} from "@/store/store";
+
+
+
+export class AuthModel {
+    async registerUser(formData: FormData) {
+        return new Promise((resolve, reject) => {
+            axios.post(`${apiConf.endpoint}/api/register`,
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(async (res) => {
+                console.log(res)
+                resolve(true);
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+
+    async loginUser(formData: FormData) {
+        return new Promise((resolve, reject) => {
+            axios.post(`${apiConf.endpoint}/api/login`,
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(async (res) => {
+                console.log(res)
+                resolve(true);
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+
+    async registerAdmin(formData: FormData) {
+        return new Promise((resolve, reject) => {
+            axios.post(`${apiConf.endpoint}/api/registerAdmin`,
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(async (res) => {
+                console.log(res)
+                resolve(true);
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+
+    async loginAdmin(formData: FormData) {
+        return new Promise((resolve, reject) => {
+            axios.post(`${apiConf.endpoint}/api/loginAdmin`,
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(async (res) => {
+                console.log(res)
+                resolve(true);
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+}
+
+export class HeadAuth extends ApiResolver {
     constructor() {
         super('api');
     }
 
     registerUser(data: FormData) {
         return this.request('register', 'POST', data)
+            .then(async (res: any) => {
+                console.log(res)
+            })
     }
 
     loginUser(data: FormData) {
